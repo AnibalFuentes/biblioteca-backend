12272bb17af1d001aeb8b123bb1647e5a5ad243c
AnibalFuentes <anibaldfuentesprieto@gmail.com>
2026-03-22 19:27:21 -0500

    setup db connect

---
+++ src/database/mongoose.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export async function connectMongo() {
+  if (isConnected) return;
+
+  const uri = process.env.db_CONN || "";
+
+  if (!uri) throw new Error("No hay conexion a la base de datos de MongoDB");
+
+  await mongoose.set("strictQuery", true);
+  await mongoose.connect(uri);
+
+  isConnected = true;
+  console.log("conectadi a MongoDB");
+
+  mongoose.connection.on("error", (err) => {
+    console.error(err);
+  });
+}
